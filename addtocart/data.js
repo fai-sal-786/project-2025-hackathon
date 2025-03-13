1b5b26a24ce231fd012e2a4c377373ab0410f2e3
fai-sal-786 <154748475+fai-sal-786@users.noreply.github.com>
2025-03-13 16:50:18 -0700

    project-hackathon-2025

---
+++ addtocart/data.js
@@ -0,0 +1,87 @@
+let shopItemsData = [
+    {
+    "id": 1,
+    "name": "Cartier La Panthere Legere Edp Perfume For Wome...",
+    "price": 51.5,
+    "desc": "women's perfumes",
+    "img": "https://perfumeonline.pk/cdn/shop/files/Cartier-Photoroom.webp?v=1709038400",
+    
+}, {
+    "id": 2,
+    "name": "Davidoff Cool Water Edt Perfume For Men 200Ml",
+    "price": 39.5,
+    "desc": "men's perfumes",
+    "img": "https://perfumeonline.pk/cdn/shop/files/DavidOff_400x.webp?v=1709038583",
+   
+},{
+    "id": 3,
+    "name": "Boss In Motion EDT Perfume 90ML best version lavish-style",
+    "price": 65,
+    "desc": "men's perfumes",
+    "img": "https://perfumeonline.pk/cdn/shop/files/Hugo_Boss_440d24ad-7e9c-467a-b122-7dc3c277c089_400x.webp?v=1709038666",
+
+},  {
+    "id": 4,
+    "name": "Dunhill Desire Gold Edt Perfume For Men 100Ml perfume",
+    "price": 45,
+    "desc": "men's perfumes",
+    "img": "https://perfumeonline.pk/cdn/shop/files/Dunhill_a4a7bb32-5f6f-4fda-9bff-6eb6f41e5a83_500x.webp?v=1709039053"
+    
+}, {
+    "id": 5,
+    "name": "Blue Enigma 3pc -The Great Masters (TGM)-WORLD-HIGH-CLASS", 
+    "price": 36.5,
+    "desc": "men clothes",
+    "img": "https://cdn.shopify.com/s/files/1/2337/7003/files/00f8057a646cdde3f6c0a0ec8699d086_5cc720b2-cd00-4d54-aa44-6119cfce9a62.jpg?v=1737353051&width=300"
+    
+}, {
+    "id": 6,
+    "name": "Muraqsh Camel on White - 3 Piece_MUGAL-STYLE-UNIQUE-class", 
+    "price": 37.5,
+    "desc": "men clothes",
+    "img": "https://cdn.shopify.com/s/files/1/2337/7003/files/0004_UJS08012.jpg?v=1722487101&width=300"   
+}, {
+    "id": 7,
+    "name": "Eminence 3 PC - GOLD ON ICED COFF-mens_clothes-lavish-class",
+    "price": 31.5,
+    "desc": "men clothes",
+    "img": "https://cdn.shopify.com/s/files/1/2337/7003/files/cc673bcadaddcf1252caab5fb4009d69.jpg?v=1716784801&width=300"
+  
+}, {
+    "id": 8,
+    "name": "Buraq Hashmat mens_clothes-lavish-unique-style-smart-version",
+    "price": 44.5,
+    "desc": "men clothes",
+    "img": "https://cdn.shopify.com/s/files/1/2337/7003/files/cropped_image-173048364312283_65dbd1ae-008e-4215-b5e7-21cb2de98949.jpg?v=1730545950&width=300"
+   
+},{
+    "id": 9,
+    "name": "iPhone 15 Pro The Ultimate iPhone.lavish-phone",
+    "price": 1999,
+    "desc": "MOBILE",
+    "img": "https://applepakistan.com.pk/wp-content/uploads/elementor/thumbs/iphone-compare-iphone-15-pro-202309-min-qhkwbx90og5cbovei8n5p7z6zsv6uqrnmmc0cj2xog.jpeg"
+   
+},{
+    "id": 10,
+    "name": "HP Victus 15 Core i7 13th Generation lavish-style Windows 11",
+    "price": 999,
+    "desc": "LAPTOP",
+    "img": "https://www.mega.pk/items_images/ts_26287.webp"  
+},{
+    "id": 11,
+    "name": "iPhone 15 total power Owl  Steel-body-lavish-STYLE",
+    "price": 1899,
+    "desc": "MOBILE",
+    "img": "https://applepakistan.com.pk/wp-content/uploads/elementor/thumbs/iphone-compare-iphone-15-202309-min-qhkwbosgyxtrf77ovmziks41nc0vxgu2lggn11fh8g.jpeg"
+   
+},{
+    "id": 12,
+    "name": "Panasonic handy cam lavish-style-boost-pic-quality",
+    "price": 799,
+    "desc": "CAMERA",
+    "img": "https://www.mega.pk/items_images/ts_25607.webp"
+   
+},
+]
+//https://perfumeonline.pk/cdn/shop/files/Dunhill_a4a7bb32-5f6f-4fda-9bff-6eb6f41e5a83_500x.webp?v=1709039053
+//
